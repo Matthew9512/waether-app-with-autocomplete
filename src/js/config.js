48d700be4a58e1a2d7f0fd0b1a2add9d3063dd03
Matthew9512 <mateuszmaciak95@gmail.com>
2023-03-12 13:12:06 +0100

    prod

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
+const lang = window.navigator.language.slice(0, 2).trim();
+
 export const API_LINK = `https://api.open-meteo.com/v1/forecast?`;
 export const API_SCOPE = `hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&&timezone=auto`;
 
@@ -5,7 +7,7 @@ export const _options = {
   method: 'GET',
   params: { namePrefixDefaultLangResults: 'true', languageCode: 'pl' },
   headers: {
-    'X-RapidAPI-Key': '32a6bd7e9dmsh3552baee89ed382p189bfejsne6b479110e8f',
+    'X-RapidAPI-Key': import.meta.env.VITE_API_KEY,
     'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
   },
 };
